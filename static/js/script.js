bd4b87c873f2421826e1b1c75ecf3709e0dd2f6a
JackTubby <jack200034@hotmail.com>
2021-02-10 21:32:01 +0000

    Add register template

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav({inDuration: 800, outDuration: 800});
+$(document).ready(function () {
+    $('.sidenav').sidenav({ inDuration: 800, outDuration: 800 });
+});
+
+$('#password, #confirm_password').on('keyup', function () {
+    if ($('#password').val() == $('#confirm_password').val()) {
+        $('#message').html('Password Matches').css('color', 'green');
+    } else
+        $('#message').html('Password Does Not Match').css('color', 'red');
 });
