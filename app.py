8d7748fa3d4b0ee5ea58b6977a4301959c860917
JackTubby <jack200034@hotmail.com>
2021-02-11 12:08:09 +0000

    Add Resgister functionality

--- app.py
+++ app.py
@@ -27,6 +27,26 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
