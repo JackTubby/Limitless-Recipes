fcafb6f6be731b339926fe2030409e80d648bae9
JackTubby <jack200034@hotmail.com>
2021-02-16 12:08:23 +0000

    Add Category dropdown

--- app.py
+++ app.py
@@ -114,7 +114,8 @@ def add_recipe():
         flash("Recipe Added Successfully")
         return redirect(url_for("get_recipes"))
 
-    return render_template("add_recipe.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_recipe.html", categories=categories)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({ inDuration: 800, outDuration: 800 });
     $('.collapsible').collapsible();
+    $('select').formSelect();
     $('input.autocomplete').autocomplete({
         data: {
             "Indian": null,
