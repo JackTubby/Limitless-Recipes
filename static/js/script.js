e949e29037c36ccc06321147916dbe2f6ac15351
JackTubby <jack200034@hotmail.com>
2021-03-09 14:11:44 +0000

    Add logic to add ingredient/step input fields

--- static/js/script.js
+++ static/js/script.js
@@ -24,12 +24,51 @@ function countChar(val) {
     }
 };
 
-/*
-$('#password, #confirm_password').on('keyup', function () {
-    if ($('#password').val() == $('#confirm_password').val()) {
-        $('#message').html('Password Matches').css('color', 'green');
-    } else
-        $('#message').html('Password Does Not Match').css('color', 'red');
-});*/
 
-// Save Button 
+// Add ingredient functions // 
+
+function add_ingredient() {
+    let newInputHTML = $(document.createElement('div')).attr("id", 'recipe_ingredient_' + ingredientCounter);
+    newInputHTML.after().html('<input type="text" name="recipe_ingredient" + id="ingredient_' + ingredientCounter + '" value="" required>');
+    newInputHTML.appendTo("#ingredient-inputs");
+    ingredientCounter ++;
+}
+
+function remove_ingredient() {
+    if (ingredientCounter > 1) {
+        $('#ingredient-inputs > div:last-child').remove();
+        ingredientCounter --;
+    } else {
+        $('#ingredient-inputs div').children('input').val('');
+    }
+}
+
+function reset_ingredients() {
+    $('#ingredient-inputs > div').not(':first').remove();
+    $('#ingredient-inputs div').children('input').val('');
+    ingredientCounter = 1;
+}
+
+// Add steps functions // 
+
+function add_step() {
+    let newInputHTML = $(document.createElement('div')).attr("id", 'recipe_step_' + stepCounter);
+    newInputHTML.after().html('<input type="text" name="recipe_method" + id="step_' + stepCounter + '" value="" required>');
+    newInputHTML.appendTo("#method-steps");
+    stepCounter ++;
+}
+
+function remove_step() {
+    if (stepCounter > 1) {
+        $('#method-steps > div:last-child').remove();
+        stepCounter --;
+    } else {
+        $('#method-steps div').children('textarea').val('');
+    }
+}
+
+function reset_steps() {
+    $('#method-steps > div').not(':first').remove();
+    $('#method-steps div').children('input').val('');
+    stepCounter = 1;
+}
