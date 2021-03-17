b7a96157d327ca4d850f2d72f1fa25b17d992a57
JackTubby <jack200034@hotmail.com>
2021-03-17 12:42:04 +0000

    Add script for dropdown-trigger

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-    $('.sidenav').sidenav({ inDuration: 800, outDuration: 800});
+    $('.sidenav').sidenav({ inDuration: 800, outDuration: 800 });
+    $(".dropdown-trigger").dropdown(
+        {
+            'closeOnClick': false
+        });
     $('.collapsible').collapsible();
     $('select').formSelect();
     $('.modal').modal();
@@ -31,13 +35,13 @@ function add_ingredient() {
     let newInputHTML = $(document.createElement('div')).attr("id", 'recipe_ingredient_' + ingredientCounter);
     newInputHTML.after().html('<input type="text" name="recipe_ingredient" + id="ingredient_' + ingredientCounter + '" value="" required>');
     newInputHTML.appendTo("#ingredient-inputs");
-    ingredientCounter ++;
+    ingredientCounter++;
 }
 
 function remove_ingredient() {
     if (ingredientCounter > 1) {
         $('#ingredient-inputs > div:last-child').remove();
-        ingredientCounter --;
+        ingredientCounter--;
     } else {
         $('#ingredient-inputs div').children('input').val('');
     }
@@ -55,13 +59,13 @@ function add_step() {
     let newInputHTML = $(document.createElement('div')).attr("id", 'recipe_step_' + stepCounter);
     newInputHTML.after().html('<input type="text" name="recipe_method" + id="step_' + stepCounter + '" value="" required>');
     newInputHTML.appendTo("#method-steps");
-    stepCounter ++;
+    stepCounter++;
 }
 
 function remove_step() {
     if (stepCounter > 1) {
         $('#method-steps > div:last-child').remove();
-        stepCounter --;
+        stepCounter--;
     } else {
         $('#method-steps div').children('textarea').val('');
     }
