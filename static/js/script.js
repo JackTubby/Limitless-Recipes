3f6690e7e33ed62b4be7d632b3098398f99ffacc
JackTubby <jack200034@hotmail.com>
2021-04-07 18:28:25 +0000

    Remove char countdown, add edit recipe add ingredient/step script

--- static/js/script.js
+++ static/js/script.js
@@ -22,17 +22,6 @@ $(document).ready(function () {
     });
 });
 
-// https://stackoverflow.com/questions/5371089/count-characters-in-textarea
-// Add Recipe Char Countdown
-function countChar(val) {
-    var len = val.value.length;
-    if (len >= 500) {
-        val.value = val.value.substring(0, 49);
-    } else {
-        $('#charNum').text(49 - len);
-    }
-}
-
 // Limit amount of characters for recipe inputs
 var max_chars = 3;
 
@@ -76,7 +65,7 @@ $(document).ready(function () {
 
     $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
         e.preventDefault(); $(this).parent('div').remove(); x--;
-    })
+    });
 });
 
 // Step input addition //
@@ -96,7 +85,47 @@ $(document).ready(function () {
 
     $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
         e.preventDefault(); $(this).parent('div').remove(); x--;
-    })
+    });
+});
+
+// Edit ingredient input addition //
+$(document).ready(function () {
+    var max_fields = 5; //maximum input boxes allowed
+    var wrapper = $(".edit_input_fields_wrap_ingredient"); //Fields wrapper
+    var add_button = $(".edit_add_field_button_ingredient"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function (e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" id="recipe_method" name="recipe_method" placeholder="Add Step" minlength="10" maxlength="300" required/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    });
+});
+
+// Edit step input addition //
+$(document).ready(function () {
+    var max_fields = 5; //maximum input boxes allowed
+    var wrapper = $(".edit_input_fields_wrap_step"); //Fields wrapper
+    var add_button = $(".edit_add_field_button_step"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function (e) { //on add input button click
+        e.preventDefault();
+        if (x < max_fields) { //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div><input type="text" id="recipe_method" name="recipe_method" placeholder="Add Step" minlength="10" maxlength="300" required/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    });
 });
 
 // To top button view page //
