e8e77b5c0d40dd8dbf269212b9fa7dd19c64c3f9
JackTubby <jack200034@hotmail.com>
2021-04-12 10:27:25 +0000

    Set debug=False

--- app.py
+++ app.py
@@ -431,4 +431,4 @@ def delete_review(recipe_id, review_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
